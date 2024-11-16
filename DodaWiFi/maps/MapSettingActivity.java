4e7aeb06e44fb9574a126cff6d80cb8f88309a19
Mizuki-Ichikawa <1chanmizu@gmail.com>
2024-11-16 17:08:15 +0900

    各ページに戻るメソッド追加

--- DodaWiFi/maps/MapSettingActivity.java
+++ DodaWiFi/maps/MapSettingActivity.java
@@ -1,12 +1,50 @@
-package com.example.ddwifi4; // パッケージ名はプロジェクトに合わせて変更
+package com.example.ddwifi4;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.ImageButton;
+import android.content.Intent;
+import android.view.View;
+
 public class MapSettingActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapsetting); // レイアウトを設定
+
+        // 掲示板ページに戻るメソッド
+        ImageButton btnReturnToBoard = findViewById(R.id.btnReturnToBoard);
+        btnReturnToBoard.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MapSettingActivity.this, Database.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // ホーム（通信ページ）に戻るメソッド
+        ImageButton btnReturnToHome = findViewById(R.id.btnReturnToHome);
+        btnReturnToHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MapSettingActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // 地図ページに戻るメソッド
+        ImageButton btnReturnToMap = findViewById(R.id.btnReturnToMap);
+        btnReturnToMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MapSettingActivity.this, MapActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
